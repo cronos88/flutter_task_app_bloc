ec4076d15b1136dea7bbcad3efda4ed0d22733b7
cronos88 <atlantis9@hotmail.com>
2022-11-06 22:36:41 -0500

    Usando Hydratred Bloc para guardar el estado del tema de la app

--- lib/blocs/switch/switch_bloc.dart
+++ lib/blocs/switch/switch_bloc.dart
@@ -1,10 +1,11 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+
+import '../bloc_exports.dart';
 
 part 'switch_event.dart';
 part 'switch_state.dart';
 
-class SwitchBloc extends Bloc<SwitchEvent, SwitchState> {
+class SwitchBloc extends HydratedBloc<SwitchEvent, SwitchState> {
   SwitchBloc() : super(const SwitchInitial(switchValue: false)) {
     on<SwitchOnEvent>((event, emit) {
       emit(
@@ -16,5 +17,15 @@ class SwitchBloc extends Bloc<SwitchEvent, SwitchState> {
         const SwitchState(switchValue: false),
       );
     });
+  }
+
+  @override
+  SwitchState? fromJson(Map<String, dynamic> json) {
+    return SwitchState.fromMap(json);
+  }
+
+  @override
+  Map<String, dynamic>? toJson(SwitchState state) {
+    return state.toMap();
   }
 }
--- lib/blocs/switch/switch_state.dart
+++ lib/blocs/switch/switch_state.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'switch_bloc.dart';
 
 class SwitchState extends Equatable {
@@ -6,6 +7,18 @@ class SwitchState extends Equatable {
 
   @override
   List<Object> get props => [switchValue];
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'switchValue': switchValue,
+    };
+  }
+
+  factory SwitchState.fromMap(Map<String, dynamic> map) {
+    return SwitchState(
+      switchValue: map['switchValue'] as bool,
+    );
+  }
 }
 
 class SwitchInitial extends SwitchState {
